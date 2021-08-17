7dbb2954ac4116b592afc77dfcbd2b9205004a67
jennafritz <jennafritz24@gmail.com>
2021-08-17 16:44:54 -0400

    Completed

--- javascript/rotate_array.js
+++ javascript/rotate_array.js
@@ -1,5 +1,17 @@
 function rotateArray(arr, k) {
   // type your code here
+
+  if(k === arr.length || arr.length === 0){
+    return arr
+  } else {
+    for(i = 0; i < k; i++){
+      let popped = arr.pop()
+      arr.unshift(popped)
+    }
+  }
+
+  return arr
+
 }
 
 if (require.main === module) {
@@ -7,12 +19,14 @@ if (require.main === module) {
   console.log("Expecting: [4, 1, 2, 3]");
   console.log("=>", rotateArray([1, 2, 3, 4], 1));
 
-  console.log("");
+  console.log("[7, 8, 9, 10, 1, 2, 3, 4, 5, 6]");
+  console.log("=>", rotateArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4));
 
   console.log("Expecting: [2, 3, 1]");
   console.log("=>", rotateArray([1, 2, 3], 2));
 
-  console.log("");
+  console.log("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]");
+  console.log("=>", rotateArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10));
 
   console.log("Expecting: [1, 2, 3]");
   console.log("=>", rotateArray([1, 2, 3], 3));
@@ -21,4 +35,12 @@ if (require.main === module) {
 module.exports = rotateArray;
 
 // Please add your pseudocode to this file
+  // if k is equal to array length or array is empty
+    // return array
+  // otherwise
+    // k times: 
+      // pop the last element off the array
+      // unshift it back to the front of the array
+  // return the now-shifted array
+  
 // And a written explanation of your solution
--- ruby/rotate_array.rb
+++ ruby/rotate_array.rb
@@ -1,5 +1,15 @@
 def rotate_array(arr, k)
   # type your code in here
+
+  if k === arr.length || arr.length === 0
+    return arr
+  else
+    k.times do
+      popped = arr.pop()
+      arr.unshift(popped)
+    end
+  end
+  arr
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -7,13 +17,17 @@ if __FILE__ == $PROGRAM_NAME
   print "=> "
   print rotate_array([1, 2, 3, 4], 1)
 
-  puts
+  puts "Expecting: [7, 8, 9, 10, 1, 2, 3, 4, 5, 6"
+  print "=> "
+  print rotate_array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4)
 
   puts "Expecting: [2, 3, 1]"
   print "=> "
   print rotate_array([1, 2, 3], 2)
 
-  puts
+  puts "Expecting: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
+  print "=> "
+  print rotate_array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10)
 
   puts "Expecting: [1, 2, 3]"
   print "=> "
@@ -22,4 +36,12 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+  # if k is equal to array length or array is empty
+    # return array
+  # otherwise
+    # k times: 
+      # pop the last element off the array
+      # unshift it back to the front of the array
+  # return the now-shifted array
+
 # And a written explanation of your solution
